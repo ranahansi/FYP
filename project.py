16ef4478a3329dc1ea50af9dd9820533250b4c90
ranahansi <34572560+ranahansi@users.noreply.github.com>
2019-06-14 12:02:04 +0530

    Main project file

---
+++ project.py
@@ -0,0 +1,50 @@
+import numpy as np
+import re
+import nltk
+from sklearn.datasets import load_files
+import pickle
+from nltk.corpus import stopwords
+
+#importing dataset
+trascript_data = load_files(r'D:\Level 4 Semester 1\project\Demo\resources')
+X, y = trascript_data.data, trascript_data.target
+
+#preprocessing
+documents = []
+
+from nltk.stem import WordNetLemmatizer
+
+stemmer = WordNetLemmatizer()
+
+for sen in range(0, len(X)):
+    # Remove all the special characters
+    document = re.sub(r'\W', ' ', str(X[sen]))
+
+    # remove all single characters
+    document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+
+    # Remove single characters from the start
+    document = re.sub(r'\^[a-zA-Z]\s+', ' ', document)
+
+    # Substituting multiple spaces with single space
+    document = re.sub(r'\s+', ' ', document, flags=re.I)
+
+    # Removing prefixed 'b'
+    document = re.sub(r'^b\s+', '', document)
+
+    # Converting to Lowercase
+    document = document.lower()
+
+    # Lemmatization
+    document = document.split()
+
+    document = [stemmer.lemmatize(word) for word in document]
+    document = ' '.join(document)
+
+    documents.append(document)
+
+#Converting Text to Numbers
+#Bag of Words
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+X = vectorizer.fit_transform(documents).toarray()
