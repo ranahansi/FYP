4a04a5cd2cf5bd3c78e3db2e506a4d3ab7913232
HanC <hansi.rane@gmail.com>
2019-06-16 17:43:41 +0530

    Sentence Tokenization

--- project.py
+++ project.py
@@ -1,46 +1,7 @@
-import numpy as np
-import re
 import nltk
-from sklearn.datasets import load_files
-import pickle
-from nltk.corpus import stopwords
 
-#importing dataset
-trascript_data = load_files(r'D:\Level 4 Semester 1\project\data-sets\2.Principles and Words-20190603T102214Z-001\2.Principles and Words')
-X, y = trascript_data.data, trascript_data.target
-
-#preprocessing
-documents = []
-
-from nltk.stem import WordNetLemmatizer
-
-stemmer = WordNetLemmatizer()
-
-for sen in range(0, len(X)):
-    # Remove all the special characters
-    document = re.sub(r'\W', ' ', str(X[sen]))
-
-    # remove all single characters
-    document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
-
-    # Remove single characters from the start
-    document = re.sub(r'\^[a-zA-Z]\s+', ' ', document)
-
-    # Substituting multiple spaces with single space
-    document = re.sub(r'\s+', ' ', document, flags=re.I)
-
-    # Removing prefixed 'b'
-    document = re.sub(r'^b\s+', '', document)
-
-    # Converting to Lowercase
-    document = document.lower()
-
-    # Lemmatization
-    document = document.split()
-
-    document = [stemmer.lemmatize(word) for word in document]
-    document = ' '.join(document)
-
-    documents.append(document)
-
-print("hello")
+# Text Tokenization
+fileObj = open('D:/Level 4 Semester 1/project/FYP/dataset/1.txt', 'r', encoding="mbcs")
+text = fileObj.read()
+tokens = nltk.sent_tokenize(text)
+print(tokens)
