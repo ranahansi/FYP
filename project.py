fb60fc0bbc0be171e6671558ab8efeb5888c11fe
ranahansi <hansi.rane@gmail.com>
2019-06-16 20:28:18 +0530

    Output wrote in txt document

--- project.py
+++ project.py
@@ -1,12 +1,10 @@
 import nltk
 from nltk.corpus import stopwords
-import fpdf
 
 # Text tokenization.
 fileObj = open('D:/Level 4 Semester 1/project/FYP/dataset/1.txt', 'r', encoding="mbcs")
 text = fileObj.read()
 words = nltk.word_tokenize(text)
-
 
 # Identifying stops words in english.
 stopWords = set(stopwords.words('english'))
@@ -22,4 +20,6 @@ for w in words:
 n = 10
 final = [wordsFiltered[i * n:(i + 1) * n] for i in range((len(wordsFiltered) + n - 1) // n)]
 
-print(final)
+with open('output.txt', 'w') as f:
+    for item in final:
+        f.write("%s\n" % item)
