0bc1b9d3c248a60f1c0306a8b8b51eed378ace5e
ranahansi <hansi.rane@gmail.com>
2019-09-09 12:07:17 +0530

    Key phrasses identification with their rank

--- project.py
+++ project.py
@@ -38,11 +38,83 @@ def extract_candidate_words(text, good_tags=set(['JJ','JJR','JJS','NN','NNP','NN
     return candidates
 
 
+def score_keyphrases_by_tfidf(texts, candidates='chunks'):
+    import gensim, nltk
+
+    # extract candidates from each text in texts, either chunks or words
+    if candidates == 'chunks':
+        boc_texts = [extract_candidate_chunks(text) for text in texts]
+    elif candidates == 'words':
+        boc_texts = [extract_candidate_words(text) for text in texts]
+    # make gensim dictionary and corpus
+    dictionary = gensim.corpora.Dictionary(boc_texts)
+    corpus = [dictionary.doc2bow(boc_text) for boc_text in boc_texts]
+    # transform corpus with tf*idf model
+    tfidf = gensim.models.TfidfModel(corpus)
+    corpus_tfidf = tfidf[corpus]
+
+    return corpus_tfidf, dictionary
+
+
+def score_keyphrases_by_textrank(text, n_keywords=0.05):
+    from itertools import takewhile, tee
+    import networkx, nltk
+
+    # tokenize for all words, and extract *candidate* words
+    words = [word.lower()
+             for sent in nltk.sent_tokenize(text)
+             for word in nltk.word_tokenize(sent)]
+    candidates = extract_candidate_words(text)
+    # build graph, each node is a unique candidate
+    graph = networkx.Graph()
+    graph.add_nodes_from(set(candidates))
+
+    # iterate over word-pairs, add unweighted edges into graph
+    def pairwise(iterable):
+        """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
+        a, b = tee(iterable)
+        next(b, None)
+        return zip(a, b)
+
+    for w1, w2 in pairwise(candidates):
+        if w2:
+            graph.add_edge(*sorted([w1, w2]))
+    # score nodes using default pagerank algorithm, sort by score, keep top n_keywords
+    ranks = networkx.pagerank(graph)
+    if 0 < n_keywords < 1:
+        n_keywords = int(round(len(candidates) * n_keywords))
+    word_ranks = {word_rank[0]: word_rank[1]
+                  for word_rank in sorted(ranks.items(), key=lambda x: x[1], reverse=True)[:n_keywords]}
+    keywords = set(word_ranks.keys())
+    # merge keywords into keyphrases
+    keyphrases = {}
+    j = 0
+    for i, word in enumerate(words):
+        if i < j:
+            continue
+        if word in keywords:
+            kp_words = list(takewhile(lambda x: x in keywords, words[i:i + 10]))
+            avg_pagerank = sum(word_ranks[w] for w in kp_words) / float(len(kp_words))
+            keyphrases[' '.join(kp_words)] = avg_pagerank
+            # counter as hackish way to ensure merged keyphrases are non-overlapping
+            j = i + len(kp_words)
+
+    return sorted(keyphrases.items(), key=lambda x: x[1], reverse=True)
+
+
 def test():
     fileObj = open('dataset/1.txt', 'r', encoding="mbcs")
     text = fileObj.read()
-    print(extract_candidate_chunks(text))
-    print(extract_candidate_words(text))
+    result = score_keyphrases_by_tfidf(text)
+    print(score_keyphrases_by_textrank(text))
+    # new_topics = result[0]
+    #
+    # for topic in new_topics:
+    #     print(topic)
+    # new_topics2 = result[1]
+    #
+    # for topic in new_topics2:
+    #     print(topic)
     # file = open("output.txt", "w")
     # file.close()
 
