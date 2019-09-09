93302faa483b72b0e93de7c2fea91db1f4d3086f
ranahansi <hansi.rane@gmail.com>
2019-09-09 10:55:18 +0530

    candidate keyphrases identification and cleaning and filtering code

--- project.py
+++ project.py
@@ -1,25 +1,51 @@
-import nltk
+import itertools, nltk, string
 from nltk.corpus import stopwords
 
-# Text tokenization.
-fileObj = open('D:/Level 4 Semester 1/project/FYP/dataset/1.txt', 'r', encoding="mbcs")
-text = fileObj.read()
-words = nltk.word_tokenize(text)
 
-# Identifying stops words in english.
-stopWords = set(stopwords.words('english'))
-wordsFiltered = []
+def extract_candidate_chunks(text, grammar=r'KT: {(<JJ>* <NN.*>+ <IN>)? <JJ>* <NN.*>+}'):
+    import itertools, nltk, string
 
-# Filtering stopwords.
-for w in words:
-    if w not in stopWords:
-        wordsFiltered.append(w)
+    # exclude candidates that are stop words or entirely punctuation
+    punct = set(string.punctuation)
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    # tokenize, POS-tag, and chunk using regular expressions
+    chunker = nltk.chunk.regexp.RegexpParser(grammar)
+    tagged_sents = nltk.pos_tag_sents(nltk.word_tokenize(sent) for sent in nltk.sent_tokenize(text))
+    all_chunks = list(itertools.chain.from_iterable(nltk.chunk.tree2conlltags(chunker.parse(tagged_sent))
+                                                    for tagged_sent in tagged_sents))
+    # join constituent chunk words into a single chunked phrase
+    candidates = [' '.join(word for word, pos, chunk in group).lower()
+                  for key, group in itertools.groupby(all_chunks, lambda word__pos__chunk: word__pos__chunk != 'O') if key]
 
-# Chunking words in equal word count
-# n = no.of words
-n = 10
-final = [wordsFiltered[i * n:(i + 1) * n] for i in range((len(wordsFiltered) + n - 1) // n)]
+    return [cand for cand in candidates
+            if cand not in stop_words and not all(char in punct for char in cand)]
 
-with open('output.txt', 'w') as f:
-    for item in final:
-        f.write("%s\n" % item)
+
+def extract_candidate_words(text, good_tags=set(['JJ','JJR','JJS','NN','NNP','NNS','NNPS'])):
+    import itertools, nltk, string
+
+    # exclude candidates that are stop words or entirely punctuation
+    punct = set(string.punctuation)
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    # tokenize and POS-tag words
+    tagged_words = itertools.chain.from_iterable(nltk.pos_tag_sents(nltk.word_tokenize(sent)
+                                                                    for sent in nltk.sent_tokenize(text)))
+    # filter on certain POS tags and lowercase all words
+    candidates = [word.lower() for word, tag in tagged_words
+                  if tag in good_tags and word.lower() not in stop_words
+                  and not all(char in punct for char in word)]
+
+    return candidates
+
+
+def test():
+    fileObj = open('dataset/1.txt', 'r', encoding="mbcs")
+    text = fileObj.read()
+    print(extract_candidate_chunks(text))
+    print(extract_candidate_words(text))
+    # file = open("output.txt", "w")
+    # file.close()
+
+
+if __name__ == "__main__":
+    test()
